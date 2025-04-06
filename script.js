04c9dfbb9c6e63b4d0d293df02cf8c7c79b90595
CHughes13 <82895994+CHughes13@users.noreply.github.com>
2025-04-06 16:42:15 +0100

    Add (and fix) displayError message function

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 // Restaurant data app using postcode search
 
 const postcodeSearchForm = document.querySelector(".postcodeSearchForm");
-const postcodeInput = document.querySelector("postcodeInput");
-const restaurantCard = document.querySelector("restaurantCard");
+const postcodeInput = document.querySelector(".postcodeInput");
+const restaurantCard = document.querySelector(".restaurantCard");
 
-postcodeSearchForm.addEventListener("submit", event => {preventDefault();
+postcodeSearchForm.addEventListener("submit", event => {
+  event.preventDefault(); // Prevents form refreshing page after hitting "submit"
+  const postcode = postcodeInput.value;
 
+  if (postcode) {
+    try{
+
+    }
+
+  } else {
+    displayError("Please enter a valid postcode");
+  }
 });
 
 // Function to return postcode restaurant data from Just Eat API
-async function fetchData(){
-    try{
 
-    //NOTE TO SELF: Need to add in validators so users can only enter valid postcodes
-        const postcode = document.getElementById("postcode").value.toString().trim().toLowerCase(); //maybe do no spaces either side or in the middle or limit number of characters//
-        
-        // Fetch API data
-        const response = await fetch("https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}");
+async function getRestaurantData(postcode) {}
 
-            if(!response.ok){
-                throw new Error("Could not fetch resource");
-            }
+function displayRestaurantData(data) {}
 
-            const data = await response.json();
+// Display error message is postcode imput is invalid
+function displayError(message) {
+  const errorDisplay = document.createElement("p");
+  errorDisplay.textContent = message;
+  errorDisplay.classList.add("errorDisplay");
 
-            const restaurant = data.restaurant[0,10]; // Limit response to first 10 restaurants
-
-// NOTE TO SELF: This section needs
-            document.getElementById("restaurantName").textContent = restaurant.name;
-            document.getElementById("cuisine").textContent = restaurant.cuisine;
-            document.getElementById("rating").textContent = restaurant.rating;
-            document.getElementById("address").textContent = restaurant.address.firstLine;
-        }
-        catch(error){
-            console.error(error);
-        }
-
+  restaurantCard.textContent = ""; // Resets form input to blank
+  restaurantCard.style.display = "flex"; // Makes restaurantCard visible to user
+  restaurantCard.appendChild(errorDisplay); // Shows user an error message
 }
-
-// Call fetchData function 
-fetchData();
