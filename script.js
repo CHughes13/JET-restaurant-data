3dc44bf54c60f08656e71c37f7cad149d1db026c
CHughes13 <82895994+CHughes13@users.noreply.github.com>
2025-04-06 18:25:09 +0100

    Add function to fetch data from API: is fetching but not displaying

--- script.js
+++ script.js
@@ -4,27 +4,51 @@ const postcodeSearchForm = document.querySelector(".postcodeSearchForm");
 const postcodeInput = document.querySelector(".postcodeInput");
 const restaurantCard = document.querySelector(".restaurantCard");
 
-postcodeSearchForm.addEventListener("submit", event => {
+// Handle postcode search form submission:
+// fetch and display restaurant data or display error message
+postcodeSearchForm.addEventListener("submit", async event => {
   event.preventDefault(); // Prevents form refreshing page after hitting "submit"
   const postcode = postcodeInput.value;
 
-  if (postcode) {
-    try{
-
+    if (postcode){
+        try{
+            const restaurantData = await getRestaurantData(postcode);
+            displayRestaurantData(restaurantData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
     }
-
-  } else {
+    else{
     displayError("Please enter a valid postcode");
   }
 });
 
 // Function to return postcode restaurant data from Just Eat API
+async function getRestaurantData(postcode) {
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"; // Free public proxy (limited use) to test fetch
+    const apiUrl = `https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`;
+    const response = await fetch(proxyUrl + apiUrl, {
+        headers: {
+            'Origin': 'localhost'
+        }
+    });
+    
+    if(!response.ok){
+        console.error("Error fetching data:", response.statusText); // Debugging line
+        throw new Error("Could not fetch restaurant data");
+    }
 
-async function getRestaurantData(postcode) {}
+    const data = await response.json() //Converts response to a JSON format
+    console.log("Fetched data:", data); // Debugging line
+    return data;
+}
 
+// Function to display restaurant data on the page
 function displayRestaurantData(data) {}
 
-// Display error message is postcode imput is invalid
+// Function to sisplay error message if postcode input is invalid
 function displayError(message) {
   const errorDisplay = document.createElement("p");
   errorDisplay.textContent = message;
