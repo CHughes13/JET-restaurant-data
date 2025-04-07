53873be64e9e527a9b99a35d1ecc692235b0d07d
CHughes13 <82895994+CHughes13@users.noreply.github.com>
2025-04-07 18:47:34 +0200

    Fix Javascript code and merge fetch.js into script.js

--- script.js
+++ script.js
@@ -1,51 +1,12 @@
-// Restaurant data app using postcode search
+// Fetch Restaurant Data
 
-const postcodeSearchForm = document.querySelector(".postcodeSearchForm");
-const postcodeInput = document.querySelector(".postcodeInput");
-const restaurantCard = document.querySelector(".restaurantCard");
+import restaurantData from "./restaurants.json";
 
-// Handle postcode search form submission:
-// fetch and display restaurant data or display error message
-postcodeSearchForm.addEventListener("submit", async event => {
-  event.preventDefault(); // Prevents form refreshing page after hitting "submit"
-  const postcode = postcodeInput.value;
+// Loop through the first 10 restaurant results only
+restaurantData.restaurants.slice(0,10).forEach(restaurant => {
 
-    if (postcode){
-        try{
-            const restaurantData = await getRestaurantData(postcode);
-            displayRestaurantData(restaurantData);
-        }
-        catch(error){
-            console.error(error);
-            displayError(error);
-        }
-    }
-    else{
-    displayError("Please enter a valid postcode");
-  }
-});
-
-// Function to return postcode restaurant data from Just Eat API
-async function getRestaurantData(postcode) {
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/"; // Free public proxy (limited use) to test fetch
-    const apiUrl = `https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/${postcode}`;
-    const response = await fetch(proxyUrl + apiUrl, {
-        headers: {
-            'Origin': 'localhost'
-        }
-    });
-    
-    if(!response.ok){
-        console.error("Error fetching data:", response.statusText); // Debugging line
-        throw new Error("Could not fetch restaurant data");
-    }
-
-    const data = await response.json(); //Converts response to a JSON format
-    console.log("Fetched data:", data); // Debugging line
-    return data; // Drill down into data returned
-}
-
-// Function to get only the restaurant data we want
+    // Generate restaurant cards
+// Function to get only the specific restaurant data we want
 function displayRestaurantData(data) {
     const {
         restauantName: name,
@@ -68,7 +29,7 @@ function displayRestaurantData(data) {
     ratingDisplay.textContent = `Star Rating: ${}`;
     addressDisplay.textContent = `Address: ${}`;
 
-// Style each seperate element     //Make sure these class stlyes are in the CSS
+    // Style each seperate element     //Make sure these class stlyes are in the CSS
     restaurantNameDisplay.classList.add("restaurantNameDisplay");
     cuisinesDisplay.classList.add("cuisinesDisplay");
     ratingDisplay.classList.add("ratingDisplay");
@@ -81,15 +42,4 @@ function displayRestaurantData(data) {
     restaurantCard.appendChild(addressDisplay);
 }
 
-
-
-// Function to display error message if postcode input is invalid
-function displayError(message) {
-  const errorDisplay = document.createElement("p");
-  errorDisplay.textContent = message;
-  errorDisplay.classList.add("errorDisplay");
-
-  restaurantCard.textContent = ""; // Resets form input to blank
-  restaurantCard.style.display = "flex"; // Makes restaurantCard visible to user using flexbox
-  restaurantCard.appendChild(errorDisplay); // Adds error message to restaurantCard
-}
+});
